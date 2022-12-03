c4419677cacfc1376f38e02090f31afd8e60a07c
LF <leo.fanseda@ontariotechu.net>
2022-12-03 18:06:15 -0500

    made edits to sign in form

--- server/routes/user.js
+++ server/routes/user.js
@@ -2,10 +2,10 @@ let express = require('express');
 let router = express.Router();
 let userControl = require('../controller/user');
 
-// DISPLAY REGISTRATION PAGE
-router.get('/register', userControl.displayRegister);
-
 // DISPLAY LOGIN PAGE          
 router.get('/login', userControl.displayLogin);
+
+// DISPLAY REGISTRATION PAGE
+router.get('/register', userControl.displayRegister);
 
 module.exports = router;
