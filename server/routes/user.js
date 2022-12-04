006310b34db4f89e1af34a2931ca5e07649d7b7e
LF <leo.fanseda@ontariotechu.net>
2022-12-04 02:39:51 -0500

    documentation

--- server/routes/user.js
+++ server/routes/user.js
@@ -2,19 +2,23 @@ let express = require('express');
 let router = express.Router();
 let userControl = require('../controller/user');
 
+
+// LOGIN CONTROLS
 // DISPLAY LOGIN          
 router.get('/login', userControl.displayLogin);
 
 // PROCESS LOGIN
 router.post('/login', userControl.processLogin);
 
+// REGISTRATION CONTROLS
 // DISPLAY REGISTRATION
 router.get('/register', userControl.displayRegister);
 
 // PROCESS REGISTER
 router.post('/register', userControl.processRegister);
 
-// LOGOUT
+
+// LOGOUT CONTROL
 router.get('/logout', userControl.logout);
 
 module.exports = router;
